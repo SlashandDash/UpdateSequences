f52ac8b31ee8d3cfb90b664f94e7be98bbbd152e
SlashandDash <slashinganddashing@gmail.com>
2025-10-13 17:03:59 -0400

    added bin/ obj/ to .gitignore

---
+++ buildts.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dotnet build && cp /home/slash/Code/crabgame/github/CrabGameSequencedDropGameMode/SequencedDropGameMode/bin/Debug/netstandard2.1/SequencedDropGameMode.dll /home/slash/.local/share/Steam/steamapps/common/Crab\ Game/BepInEx/plugins
