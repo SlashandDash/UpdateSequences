e5fd8f99fce858a87539a64603224ac074211434
SlashandDash <slashinganddashing@gmail.com>
2025-10-13 17:06:39 -0400

    test

--- buildts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dotnet build && cp /home/slash/Code/crabgame/github/CrabGameSequencedDropGameMode/SequencedDropGameMode/bin/Debug/netstandard2.1/SequencedDropGameMode.dll /home/slash/.local/share/Steam/steamapps/common/Crab\ Game/BepInEx/plugins
